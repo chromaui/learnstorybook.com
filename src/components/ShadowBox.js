1ba989c660583cb01d553d26b37cc1c662208701
Kyle Suss <susskyle@gmail.com>
2019-06-05 15:29:42 -0600

    Add GlobalStyle for stories, stories for CTA, ShadowBox

---
+++ src/components/ShadowBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ShadowBox = styled.div`
+  background: #ffffff;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 5px 15px 0 rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+`;
+
+export default ShadowBox;
