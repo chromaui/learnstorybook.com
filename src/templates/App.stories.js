0b17a6e6d704c94c1180eb31bb8335a3e52ddff6
Kyle Suss <susskyle@gmail.com>
2019-06-05 18:47:11 -0600

    Ignore eslint import errors in chapter stories

--- src/templates/App.stories.js
+++ src/templates/App.stories.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 import React from 'react';
 import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
