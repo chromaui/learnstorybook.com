9095909420dd170f6b0756fafab94dedbeb483c0
Kyle Suss <susskyle@gmail.com>
2019-06-12 17:18:28 -0600

    Update guide desktop content width

--- src/templates/guide.js
+++ src/templates/guide.js
@@ -27,6 +27,7 @@ const Content = styled.div`
 const Overview = styled.div`
   @media (min-width: ${breakpoint * 1.5}px) {
     margin-right: 84px;
+    width: 50%;
   }
 `;
 
@@ -44,6 +45,7 @@ const Detail = styled.div`
 
   @media (min-width: ${breakpoint * 1.5}px) {
     margin-top: 0;
+    width: 50%;
   }
 `;
 
