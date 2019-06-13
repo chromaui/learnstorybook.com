a7829d71ae7727ddf94b3810b852a47800bb2699
Kyle Suss <susskyle@gmail.com>
2019-06-12 18:15:07 -0600

    Fix homepage bg render, margin issues

--- src/templates/App.stories.js
+++ src/templates/App.stories.js
@@ -7,11 +7,12 @@ const Children = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 169px 0 100px;
 `;
 
 const props = {
   children: <Children>I am the app.</Children>,
-  location: { pathname: ':pathname' },
+  location: { pathname: '' },
 };
 
 storiesOf('Templates|App', module).add('default', () => <App {...props} />);
