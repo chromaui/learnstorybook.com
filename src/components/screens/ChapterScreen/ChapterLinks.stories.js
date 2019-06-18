990543b072e0e9019f0a62c44e15b9d55bfb27f1
Kyle Suss <susskyle@gmail.com>
2019-06-17 18:24:06 -0600

    Add ChapterLinks story for missing commit

--- src/components/screens/ChapterScreen/ChapterLinks.stories.js
+++ src/components/screens/ChapterScreen/ChapterLinks.stories.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ChapterLinks from './ChapterLinks';
 
-storiesOf('Screens|ChapterScreen/ChapterLinks', module).add('default', () => (
-  <ChapterLinks codeGithubUrl="https://github.com" commit="AAAAAA" />
-));
+storiesOf('Screens|ChapterScreen/ChapterLinks', module)
+  .add('with commit', () => <ChapterLinks codeGithubUrl="https://github.com" commit="AAAAAA" />)
+  .add('without commit', () => <ChapterLinks codeGithubUrl="https://github.com" />);
