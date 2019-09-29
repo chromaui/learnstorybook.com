e7e6e907404542eb84d8d2837aeeb02948086a64
domyen <dom@hichroma.com>
2019-09-29 17:27:26 -0400

    Added testimonials from Michele and Lee!

--- src/components/screens/IndexScreen/SocialValidation.js
+++ src/components/screens/IndexScreen/SocialValidation.js
@@ -140,22 +140,24 @@ const SocialValidation = () => (
       </Testimonial>
 
       <Testimonial>
-        Just deleted all component snapshot tests from our React component library—in favour of
-        Chromatic[@chromaui] screenshot tests.
+        I was amazed at how easy it was to build a Design System with Storybook! The guide provided
+        here is just perfect. Everything is explained step by step, from the setup of the repository
+        to the deployment, including the tests and the generation of the documentation.
         <UserWrapper
-          src="https://avatars2.githubusercontent.com/u/263385"
-          name="Dominic Nguyen"
-          detail="Engineer at X"
+          src="https://avatars2.githubusercontent.com/u/19664438"
+          name="Michèle Legait"
+          detail="Engineer at PROS"
         />
       </Testimonial>
 
       <Testimonial>
-        Just deleted all component snapshot tests from our React component library—in favour of
-        Chromatic[@chromaui] screenshot tests.
+        Design systems are rapidly growing in popularity. It can be overwhelming for developers to
+        understand where to start. This guide will kickstart your design system using industry
+        standard best practices.
         <UserWrapper
-          src="https://avatars2.githubusercontent.com/u/263385"
-          name="Dominic Nguyen"
-          detail="Engineer at Y"
+          src="https://avatars2.githubusercontent.com/u/9113740"
+          name="Lee Robinson"
+          detail="Engineer at Hy-Vee"
         />
       </Testimonial>
     </Testimonials>
