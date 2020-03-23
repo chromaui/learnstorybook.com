e64877e5c6c74e64fdfa3d12ecd350c263d42789
jonniebigodes <jonniebigodes@hotmail.com>
2020-03-23 18:31:14 +0000

    fix stories

--- src/components/screens/ChapterScreen/TableOfContents.stories.js
+++ src/components/screens/ChapterScreen/TableOfContents.stories.js
@@ -5,8 +5,21 @@ import TableOfContents from './TableOfContents';
 const currentPageSlug = '/slug-1';
 
 const entries = [
-  { slug: currentPageSlug, title: 'Chapter 1' },
-  { slug: '/slug-2', title: 'Chapter 2' },
+  {
+    slug: currentPageSlug,
+    title: 'Chapter 1',
+    chapterNavItems: [
+      {
+        chapterNavText: 'Setup',
+        chapterNavLink: '/intro-to-storybook/react/en/get-started/#setup-react-storybook',
+      },
+      {
+        chapterNavText: 'Reuse CSS',
+        chapterNavLink: '/intro-to-storybook/react/en/get-started/#reuse-css',
+      },
+    ],
+  },
+  { slug: '/slug-2', title: 'Chapter 2', chapterNavItems: [] },
 ];
 
 storiesOf('Screens|ChapterScreen/TableOfContents', module)
