898491825c6cdc09a3f3e040eba7271fb862bae7
jonniebigodes <jonniebigodes@hotmail.com>
2020-06-24 18:15:19 +0100

    bumped styled-components and removed unecessary import

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addDecorator, addParameters, configure } from '@storybook/react';
+import { addDecorator, addParameters } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import {
   global as designSystemGlobal,
