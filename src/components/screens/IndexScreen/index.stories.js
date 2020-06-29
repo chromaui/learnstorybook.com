ebeff794440f1b1985e71aa1da2f1af3e609ab7e
jonniebigodes <jonniebigodes@hotmail.com>
2020-06-29 16:52:13 +0100

    update index stories

--- src/components/screens/IndexScreen/index.stories.js
+++ src/components/screens/IndexScreen/index.stories.js
@@ -33,10 +33,38 @@ Default.args = {
         {
           node: {
             frontmatter: {
-              description: 'This is the guide description that explains what you find in the guide',
-              title: 'Guide title',
+              description: `Learn to create bulletproof UI components, along the way you'll build an app UI from scratch.`,
+              title: 'Intro to Storybook',
               themeColor: '#6F2CAC',
               thumbImagePath: '/guide-thumb/intro.svg',
+            },
+            fields: {
+              guide: 'guide',
+              slug: '/guide',
+            },
+          },
+        },
+        {
+          node: {
+            frontmatter: {
+              description: 'Discover how to build and maintain design systems using Storybook.',
+              title: 'Design Systems for Developers',
+              themeColor: '#0079FF',
+              thumbImagePath: '/guide-thumb/design-system.svg',
+            },
+            fields: {
+              guide: 'guide',
+              slug: '/guide',
+            },
+          },
+        },
+        {
+          node: {
+            frontmatter: {
+              description: '✍️Coming soon: Visual testing is a pragmatic yet precise way to check UI appearance.',
+              title: 'Visual Testing Handbook',
+              themeColor: '#129F00',
+              thumbImagePath: '/guide-thumb/visual-testing.svg',
             },
             fields: {
               guide: 'guide',
