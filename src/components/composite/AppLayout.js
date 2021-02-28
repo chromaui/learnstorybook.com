2f273dbb2c2447e74f90bec489acff0a785b9fca
winkerVSbecks <varunvachhar@gmail.com>
2021-02-28 12:08:35 -0500

    fix header variation on 404 page

--- src/components/composite/AppLayout.js
+++ src/components/composite/AppLayout.js
@@ -40,7 +40,7 @@ const query = graphql`
 const getHeaderInvertedState = pathname => {
   const pathParts = pathname.split('/').filter(p => !!p && p !== 'tutorials');
   // This will need to get "smarter" if the hierarchy of pages/guides changes.
-  return pathParts.length === 1 && pathParts[0] !== 'team' && pathParts[0] !== '404';
+  return pathParts.length === 1 && pathParts[0] !== '404';
 };
 
 const TemplateWrapper = ({ location: { pathname }, children }) => (
